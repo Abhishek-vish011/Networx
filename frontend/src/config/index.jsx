670f09302817cee4e7ffc6c3bcd4a2e8e2832d65
Abhishek-vish011 <abhivishwa012002@gmail.com>
2025-08-01 20:43:12 +0530

    edit backend link

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL = "https://networx-zdpz.onrender.com"
+export const BASE_URL = "https://networx-newserver.onrender.com"
 export const clientServer = axios.create({
     baseURL: BASE_URL,
 })
