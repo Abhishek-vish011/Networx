10d0df803203b2aae7c67e6b617033d66c7eb0f8
Abhishek-vish011 <abhivishwa012002@gmail.com>
2025-08-03 19:32:32 +0530

    Ready to frontend

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL = "http://localhost:9080"
+export const BASE_URL = "https://networx-backend-fmj2.onrender.com"
 export const clientServer = axios.create({
     baseURL: BASE_URL,
 })
